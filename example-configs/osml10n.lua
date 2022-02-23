dbb6281dfed9c90653a23cfa3e0ad1fbb3941461
Jochen Topf <jochen@topf.org>
2022-02-23 12:05:42 +0100

    Improved osml10n demo Lua code

--- example-configs/osml10n.lua
+++ example-configs/osml10n.lua
@@ -1,10 +1,23 @@
 --
 -- Use osml10n library to localize names
 --
--- This will transliterate all 'name' and 'name:*' tags. For each a new tag
--- with '_translit' appended will be set.
+-- This will transliterate various name tags:
+-- * The new tag `osml10n_latin` will contain a name suitable for a label
+--   in latin characters only. This is based on all available `name` and
+--   `name:*` tags of each object.
+-- * The new tag `osml10n_name` will contain a label with the original
+--   name and the transliterated name. Street names will get additional
+--   processing with some names abbreviated (Road -> Rd etc.). This is
+--   based on all available `name` and `name:*` tags.
+-- * The new tag `osml10n_transcription` will contain a transscribed
+--   version of the `name` tag.
 --
--- Needs https://github.com/giggls/osml10n
+-- This is intended as a demo for some of the capabilities of the osml10n
+-- library. For production use you have to adapt this to suit your needs.
+--
+-- Needs https://github.com/giggls/osml10n . Look there for more documentation,
+-- the `osml10n.*` functions are documented in the Lua files in the directory
+-- `lua_osml10/osml10n/`.
 --
 
 local osml10n = require 'osml10n.init'
@@ -12,20 +25,25 @@ local osml10n = require 'osml10n.init'
 function process(objtype, object)
     local label = osml10n.get_localized_name_from_tags(objtype .. object.id, object.tags, 'de', object.bbox)
     if label and #label > 0 then
-        object.tags['osml10n_label'] = label
+        object.tags['osml10n_latin'] = label
     end
 
     if object.tags.name then
         local trans = osml10n.geo_transcript(objtype .. object.id, object.tags.name, object.bbox)
         if trans and #trans > 0 then
-            object.tags['osml10n_name'] = trans
+            object.tags['osml10n_transcription'] = trans
         end
     end
 
     if object.tags.highway then
         local trans = osml10n.get_streetname_from_tags(objtype .. object.id, object.tags, true, false, '|', 'de', object.bbox)
         if trans and #trans > 0 then
-            object.tags['osml10n_streetname'] = trans
+            object.tags['osml10n_name'] = trans
+        end
+    else
+        local trans = osml10n.get_placename_from_tags(objtype .. object.id, object.tags, true, false, '|', 'de', object.bbox)
+        if trans and #trans > 0 then
+            object.tags['osml10n_name'] = trans
         end
     end
 
